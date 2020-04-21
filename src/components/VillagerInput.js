4c083538da936b1f30416deb312f27c9c6268421
HENRY VO <HENRYVO94@GMAIL.com>
2020-04-21 16:28:28 +1000

    Add data files and error checking
    - User can only input villagers listed in json

--- src/components/VillagerInput.js
+++ src/components/VillagerInput.js
@@ -1,12 +1,27 @@
 import React, { useState } from 'react';
 
+import villagersList from '../data/villagers';
+
 const VillagerInput = ({addVillager}) => {
   const [input, setInput] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (isDisabled() === true) {
+      return false;
+    }
+
+    console.log('add villager');
 
     addVillager(input);
+  }
+
+  const isDisabled = (name = input, list = villagersList) => {
+    if (!list.includes(name.toLowerCase())) {
+      return true;
+    }
+
+    return false;
   }
 
   return (
@@ -15,8 +30,10 @@ const VillagerInput = ({addVillager}) => {
         type="text"
         value={input}
         label="Add villager"
-        onChange={event => setInput(event.target.value)}
+        onChange={(event) => setInput(event.target.value)}
       />
+
+      <button type="button" disabled={isDisabled()}>Add villager</button>
     </form>
   );
 };
